5ade21e740233cafef319287c85bf75ae4bcd44a
Carolinav66 <37343898+Carolinav66@users.noreply.github.com>
2018-10-08 14:30:38 +0100

    A camera ortografica já está feita

--- 1ª Entrega/Game.js
+++ 1ª Entrega/Game.js
@@ -24,12 +24,12 @@ class Game {
     createCamera(x, y, z) {
         'use strict';
         
-         this.camera = new THREE.OrthographicCamera(window.innerWidth / - 10,
-                                                    window.innerWidth / 10,
-                                                    window.innerHeight / 10,
-                                                    window.innerHeight / - 10, 
-                                                    1,
-                                                    1000);
+        this.camera = new THREE.OrthographicCamera(window.innerWidth / - 10,
+                                                   window.innerWidth / 10,
+                                                   window.innerHeight / 10,
+                                                   window.innerHeight / - 10, 
+                                                   1,
+                                                   1000);
         this.camera.position.x = x;  
         this.camera.position.y = y;
         this.camera.position.z = z;
