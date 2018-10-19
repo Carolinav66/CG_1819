aaf7494984f8bab76cdbcce7b371ef60a5636d72
Carolinav66 <37343898+Carolinav66@users.noreply.github.com>
2018-10-19 15:27:09 +0100

    ROLANDO

--- 2ª Entrega/Ball.js
+++ 2ª Entrega/Ball.js
@@ -7,8 +7,8 @@ class Ball extends GraphicEntity {
             "ball"+id);
         this.radius = radius;
         var angle = Math.random()*2*Math.PI;
-        this.velocity = new THREE.Vector3(1,0,0);
         this.addBall(x,y,z,angle);
+        this.velocity = new THREE.Vector3(Math.cos(angle),0,Math.sin(angle));
     }
 
     addBall(x,y,z,angle){
@@ -16,7 +16,7 @@ class Ball extends GraphicEntity {
         var axis = new THREE.AxisHelper(6);
         var geometry = new THREE.SphereGeometry(this.radius, 15, 15);
         var mesh = new THREE.Mesh(geometry, this.material);
-        this.rotation.y=angle
+        this.rotation.y=angle;
         this.add(mesh);
         this.add(axis);
     }
@@ -34,7 +34,14 @@ class Ball extends GraphicEntity {
     }
 
     updateBall(delta){
-        var x = delta*1
-        this.translateOnAxis(this.velocity, x);
+        var x = delta*Math.PI*2;
+        this.updatePosition(this.velocity,x);
+        for (var i = 0; i < this.children.length; i++) {
+            //console.log(this.children[i]," is mesh? ",this.children[i].isMesh)
+            if (this.children[i] instanceof THREE.Mesh) {
+                console.log("ROLANDO")
+                this.children[i].rotation.z+=-delta;
+            }
+        }
     }
 }
--- 2ª Entrega/GraphicEntity.js
+++ 2ª Entrega/GraphicEntity.js
@@ -9,4 +9,16 @@ class GraphicEntity extends THREE.Object3D {
         this.material = material;
         this.name = name;
     }
+
+    updatePosition(dx,dy,dz){
+        this.position.x += dx;
+        this.position.y += dy;
+        this.position.z += dz;
+    }
+
+    updatePosition(vector,delta){
+        this.position.x += vector.x *delta;
+        this.position.y += vector.y *delta;
+        this.position.z += vector.z *delta;
+    }
 }
