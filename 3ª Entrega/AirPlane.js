f78f3f275265a35efe81077b28e1a3badea7bdfa
jaga8285 <joao.ag.andrade@gmail.com>
2018-11-03 12:16:29 +0000

    Planes

---
+++ 3ª Entrega/AirPlane.js
@@ -0,0 +1,27 @@
+class AirPlane extends GraphicEntity {
+    constructor(x, y, z) {
+        'use strict';
+
+        super(x, y, z,
+            [new THREE.MeshBasicMaterial({ color: 0xc56836 /*0xff09e5*/, wireframe: false })],
+            "field");
+
+        this.createWing();
+
+    }
+
+    createWing(){
+        /*var vertices = [new THREE.Vector3(0,0,0),
+                        new THREE.Vector3(0,0,40),
+                        new THREE.Vector3(9,0,37),
+                        new THREE.Vector3(10,0,0)]
+
+        var geometry = new THREE.Geometry();
+        geometry.vertices.push(vertices[0],vertices[1],vertices[2],vertices[3]);
+        geometry.faces.push(new THREE.Face3(0,1,2));
+        geometry.faces.push(new THREE.Face3(0,2,3));*/
+        var geometry = this.createPlane(0,0,0,10,10,4,4);
+        var mesh = new THREE.Mesh(geometry, this.materials[0]);
+        this.add(mesh);
+    }
+}
